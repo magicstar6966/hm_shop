5f177825f49c6068326e4c9983c60648226b0971
magicstar6966 <magicstar6966@gmail.com>
2025-12-11 20:18:08 +0800

    实现轮播图渲染

--- lib/components/home/hmSlider.dart
+++ lib/components/home/hmSlider.dart
@@ -1,20 +1,43 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:hm_shop/viewmodels/home.dart';
 
 class HmSlider extends StatefulWidget {
-  HmSlider({Key? key}) : super(key: key);
+  final List<BannerItem> bannerList;
+  HmSlider({Key? key, required this.bannerList}) : super(key: key);
 
   @override
   _HmSliderState createState() => _HmSliderState();
 }
 
 class _HmSliderState extends State<HmSlider> {
+  Widget _getSlider() {
+    final double screenWidth = MediaQuery.of(context).size.width;
+    return CarouselSlider(
+      items: List.generate(widget.bannerList.length, (int index) {
+        return Image.network(
+          widget.bannerList[index].imgUrl,
+          fit: BoxFit.cover,
+          width: screenWidth,
+        );
+      }),
+      options: CarouselOptions(
+        autoPlayInterval: Duration(seconds: 2),
+        height: 400,
+        viewportFraction: 1,
+        autoPlay: true,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blueAccent,
-      height: 300,
-      alignment: Alignment.center,
-       child: Text("轮播图",style: TextStyle(color: Colors.white, fontSize: 20))
-    );
+    return Stack(children: [_getSlider()]);
+    // Container(
+    //   color: Colors.blueAccent,
+    //   height: 300,
+    //   alignment: Alignment.center,
+    //    child: Text("轮播图",style: TextStyle(color: Colors.white, fontSize: 20))
+    // );
   }
 }
--- lib/pages/home/index.dart
+++ lib/pages/home/index.dart
@@ -4,6 +4,7 @@ import 'package:hm_shop/components/home/hmHot.dart';
 import 'package:hm_shop/components/home/hmMoreList.dart';
 import 'package:hm_shop/components/home/hmSlider.dart';
 import 'package:hm_shop/components/home/hmSuggestion.dart';
+import 'package:hm_shop/viewmodels/home.dart';
 
 class HomeView extends StatefulWidget {
   HomeView({Key? key}) : super(key: key);
@@ -13,11 +14,28 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  final List<BannerItem> _bannerList = [
+    BannerItem(
+      id: '1',
+      imgUrl:
+          "https://img.alicdn.com/imgextra/i1/O1CN01f0xQO3231pCQyxUMR_!!6000000007196-2-tps-2112-800.png",
+    ),
+    BannerItem(
+      id: '2',
+      imgUrl:
+          "https://img.alicdn.com/imgextra/i3/O1CN01zILgqe1OpNoGuLwC6_!!6000000001754-2-tps-2112-800.png",
+    ),
+    BannerItem(
+      id: '3',
+      imgUrl:
+          'https://img.alicdn.com/imgextra/i4/O1CN01hyRl721l7AjxUP0kt_!!6000000004771-2-tps-2112-800.png',
+    ),
+  ];
   //获取滚动容器的内容
   List<Widget> _getScrollChildren() {
     return [
       //包裹普通 widget 的 sliver 家族的组件
-      SliverToBoxAdapter(child: HmSlider()),
+      SliverToBoxAdapter(child: HmSlider(bannerList: _bannerList)),
       //放置间隔
       SliverToBoxAdapter(child: SizedBox(height: 10)),
       //放置分类组件
@@ -44,7 +62,7 @@ class _HomeViewState extends State<HomeView> {
       ),
       //放置间隔
       SliverToBoxAdapter(child: SizedBox(height: 10)),
-      HmMoreList()
+      HmMoreList(),
     ];
   }
 
